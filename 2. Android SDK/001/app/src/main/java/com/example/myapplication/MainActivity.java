9bd5e859d3c57fbbc9be707c42a98aa6cae7c0c6
Rustam Galimov <Rustam.Galimov@icl-services.com>
2019-10-29 20:56:23 +0300

    added checkstyle plugin

--- 2. Android SDK/001/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 2. Android SDK/001/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    int count = 0;
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState!=null)
+        if (savedInstanceState != null) {
             count = savedInstanceState.getInt("count");
+        }
         Log.d("onCreate", "count = " + count);
     }
 
@@ -22,6 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         ++count;
-        outState.putInt("count",count);
+        outState.putInt("count", count);
     }
 }
