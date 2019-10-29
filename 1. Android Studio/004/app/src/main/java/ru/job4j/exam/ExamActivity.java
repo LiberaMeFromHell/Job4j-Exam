afb0b7af51557096f554ec27b1eb90c0e28ff256
Rustam Galimov <Rustam.Galimov@icl-services.com>
2019-10-29 13:40:01 +0300

    5. Компоненты для ввода данных

---
+++ 1. Android Studio/004/app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -0,0 +1,20 @@
+package ru.job4j.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_layout);
+    }
+}
