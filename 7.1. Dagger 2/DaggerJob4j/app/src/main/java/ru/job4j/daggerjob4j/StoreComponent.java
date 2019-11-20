1bb0d37071a6e91bc0e2a1cd59ff74cd5835fe3a
Rustam Galimov <Rustam.Galimov@icl-services.com>
2019-11-20 15:08:35 +0300

    https://job4j.ru/TrackStudio/staticframeset.html

--- 7.1. Dagger 2/DaggerJob4j/app/src/main/java/ru/job4j/daggerjob4j/StoreComponent.java
+++ 7.1. Dagger 2/DaggerJob4j/app/src/main/java/ru/job4j/daggerjob4j/StoreComponent.java
@@ -1,7 +1,10 @@
 package ru.job4j.daggerjob4j;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
+@Singleton
 @Component(modules = StoreModule.class)
 public interface StoreComponent {
     void injectTo(MainActivity activity);
