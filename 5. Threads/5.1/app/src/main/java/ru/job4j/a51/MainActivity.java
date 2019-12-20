ce1f1277227480ca40c6c97399b612b04c8e7bdf
Rustam Galimov <Rustam.Galimov@icl-services.com>
2019-12-20 10:26:12 +0300

    02 - Main (UI) Thread, runOnUiThread [#91383]

--- 5. Threads/5.1/app/src/main/java/ru/job4j/a51/MainActivity.java
+++ 5. Threads/5.1/app/src/main/java/ru/job4j/a51/MainActivity.java
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private volatile boolean stopThread = false;
     private TestThread thread;
+    private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
     }
 
     public void startThread(View view) {
-        thread = new TestThread(10);
+        thread = new TestThread(10,textView,imageView);
         thread.start();
     }
 
--- 5. Threads/5.1/app/src/main/java/ru/job4j/a51/TestThread.java
+++ 5. Threads/5.1/app/src/main/java/ru/job4j/a51/TestThread.java
@@ -1,20 +1,39 @@
 package ru.job4j.a51;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TestThread extends Thread {
     private int times;
+    private Handler mainHandler = new Handler();
+    private TextView textView;
+    private ImageView imageView;
 
-    public TestThread(int times) {
+    public TestThread(int times, TextView textView, ImageView imageView) {
         this.times = times;
+        this.textView = textView;
+        this.imageView = imageView;
     }
 
     @Override
     public void run() {
         int count = 0;
+
+        editImageView(imageView);
+
         while (count != times) {
             Log.d("TAG", "run: " + count);
             count++;
+
+            if (count == 5) {
+                editTextView();
+            }
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -22,5 +41,25 @@ public class TestThread extends Thread {
                 break;
             }
         }
+    }
+
+    private void editTextView() {
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText("50%");
+            }
+        });
+    }
+
+    private void editImageView(final ImageView imageView) {
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                final Bitmap bitmap = BitmapFactory
+                        .decodeResource(imageView.getResources(), R.drawable.temp1);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
     }
 }
